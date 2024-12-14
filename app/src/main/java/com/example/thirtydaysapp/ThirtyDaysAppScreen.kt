06dd19757fcb201c9c158c1c84de0d3dbb37c0b6
dadothetato <dle32843@gmail.com>
2024-12-14 18:25:29 +1100

    New PushUpApp composable which runs the app.

--- app/src/main/java/com/example/thirtydaysapp/ThirtyDaysAppScreen.kt
+++ app/src/main/java/com/example/thirtydaysapp/ThirtyDaysAppScreen.kt
@@ -4,15 +4,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -22,13 +26,51 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import com.example.thirtydaysapp.data.PushUp
+import com.example.thirtydaysapp.data.PushUpRepository
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PushUpTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge
+            )
+        },
+        modifier = modifier
+    )
+}
+
+
+@Composable
+fun PushUpApp() {
+    Scaffold(
+        topBar = {
+            PushUpTopAppBar()
+        }
+    ) {
+        LazyColumn(contentPadding = it) {
+            itemsIndexed(PushUpRepository.pushUps) { index, pushUp ->
+                PushUpItem(
+                    pushUp = pushUp,
+                    dayNumber = index + 1,
+                    modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
+                )
+            }
+        }
+
+    }
+}
 
 
 @Composable
@@ -65,14 +107,14 @@ fun PushUpItem(pushUp: PushUp, dayNumber: Int, modifier: Modifier = Modifier) {
                     modifier = Modifier
                         .clip(MaterialTheme.shapes.small)
                         .background(color = Color.White)
-                        .fillMaxHeight()
+                        .fillMaxSize()
                 )
             }
         }
     }
 }
 
-
+/*
 @Preview(showBackground = true)
 @Composable
 fun PushUpItemPreview() {
@@ -80,4 +122,11 @@ fun PushUpItemPreview() {
         pushUp = PushUp(typeRes = R.string.one_arm_supported_push_up, imageRes = R.drawable.one_arm_supported_push_ups),
         dayNumber = 1
     )
+}
+ */
+
+@Preview(showBackground = true)
+@Composable
+fun PushUpAppPreview() {
+    PushUpApp()
 }
