fa7568048f794a7b0a4410b1c52c53c7738489e0
dadothetato <dle32843@gmail.com>
2024-12-14 16:42:53 +1100

    New object that contains a list that contains each pushup.

---
+++ app/src/main/java/com/example/thirtydaysapp/data/PushUpRepository.kt
@@ -0,0 +1,128 @@
+package com.example.thirtydaysapp.data
+
+import com.example.thirtydaysapp.R
+
+object PushUpRepository {
+    val pushUps = listOf(
+        PushUp(
+            typeRes = R.string.incline_push_up,
+            imageRes = R.drawable.incline_push_up
+        ),
+        PushUp(
+            typeRes = R.string.regular_push_up,
+            imageRes = R.drawable.regular_push_up
+        ),
+        PushUp(
+            typeRes = R.string.spider_man_push_up,
+            imageRes = R.drawable.spiderman_push_up
+        ),
+        PushUp(
+            typeRes = R.string.push_up_with_rotation,
+            imageRes = R.drawable.push_up_with_rotation
+        ),
+        PushUp(
+            typeRes = R.string.toe_touch_push_up,
+            imageRes = R.drawable.toe_touch_push_up
+        ),
+        PushUp(
+            typeRes = R.string.decline_push_up,
+            imageRes = R.drawable.decline_push_up
+        ),
+        PushUp(
+            typeRes = R.string.wide_grip_push_up,
+            imageRes = R.drawable.wide_push_up
+        ),
+        PushUp(
+            typeRes = R.string.close_grip_push_up,
+            imageRes = R.drawable.close_grip_push_up
+        ),
+        PushUp(
+            typeRes = R.string.diamond_push_up,
+            imageRes = R.drawable.diamond_push_up
+        ),
+        PushUp(
+            typeRes = R.string.reverse_grip_push_up,
+            imageRes = R.drawable.reverse_grip_push_up
+        ),
+        PushUp(
+            typeRes = R.string.knuckle_push_up,
+            imageRes = R.drawable.knuckle_push_up
+        ),
+        PushUp(
+            typeRes = R.string.finger_push_up,
+            imageRes = R.drawable.finger_push_up
+        ),
+        PushUp(
+            typeRes = R.string.push_up_with_push_up_bars,
+            imageRes = R.drawable.push_up_with_push_up_bars
+        ),
+        PushUp(
+            typeRes = R.string.resistance_band_push_up,
+            imageRes = R.drawable.resistance_band_push_up
+        ),
+        PushUp(
+            typeRes = R.string.cross_arm_push_up,
+            imageRes = R.drawable.cross_arm_push_up
+        ),
+        PushUp(
+            typeRes = R.string.isometric_push_up,
+            imageRes = R.drawable.isometric_push_up
+        ),
+        PushUp(
+            typeRes = R.string.sphinx_push_up,
+            imageRes = R.drawable.sphinx_push_up
+        ),
+        PushUp(
+            typeRes = R.string.hindu_push_up,
+            imageRes = R.drawable.hindu_push_up
+        ),
+        PushUp(
+            typeRes = R.string.cobra_push_up,
+            imageRes = R.drawable.cobra_push_up
+        ),
+        PushUp(
+            typeRes = R.string.plyo_push_up,
+            imageRes = R.drawable.plyo_push_up
+        ),
+        PushUp(
+            typeRes = R.string.clapping_push_up,
+            imageRes = R.drawable.clapping_push_up
+        ),
+        PushUp(
+            typeRes = R.string.archer_push_up,
+            imageRes = R.drawable.archer_push_up
+        ),
+        PushUp(
+            typeRes = R.string.typewriter_push_up,
+            imageRes = R.drawable.typewriter_push_up
+        ),
+        PushUp(
+            typeRes = R.string.one_arm_knee_push_up,
+            imageRes = R.drawable.one_arm_knee_push_up
+        ),
+        PushUp(
+            typeRes = R.string.one_arm_supported_push_up,
+            imageRes = R.drawable.one_arm_supported_push_ups
+        ),
+        PushUp(
+            typeRes = R.string.pike_push_up,
+            imageRes = R.drawable.pike_push_up
+        ),
+        PushUp(
+            typeRes = R.string.elevated_pike_push_up,
+            imageRes = R.drawable.elevated_pike_push_up
+        ),
+        PushUp(
+            typeRes = R.string.pseudo_planche_push_up,
+            imageRes = R.drawable.pseudo_planche_push_up
+        ),
+        PushUp(
+            typeRes = R.string.one_arm_push_up,
+            imageRes = R.drawable.one_arm_push_up
+        ),
+        PushUp(
+            typeRes = R.string.handstand_push_up,
+            imageRes = R.drawable.handstand_push_up
+        ),
+    )
+}
