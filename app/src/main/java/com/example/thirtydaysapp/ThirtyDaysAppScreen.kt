467d8ac6dc3521b3e00b1e93343044419fb52dee
dadothetato <dle32843@gmail.com>
2024-12-14 18:57:00 +1100

    Added checkbox.

--- app/src/main/java/com/example/thirtydaysapp/ThirtyDaysAppScreen.kt
+++ app/src/main/java/com/example/thirtydaysapp/ThirtyDaysAppScreen.kt
@@ -2,23 +2,32 @@ package com.example.thirtydaysapp
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalMinimumInteractiveComponentEnforcement
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -28,11 +37,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
 import com.example.thirtydaysapp.data.PushUp
 import com.example.thirtydaysapp.data.PushUpRepository
 
@@ -73,11 +77,13 @@ fun PushUpApp() {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PushUpItem(pushUp: PushUp, dayNumber: Int, modifier: Modifier = Modifier) {
     val pushUpType = stringResource(pushUp.typeRes)
     val pushUpNumber = 100
     val itemText = "Day $dayNumber: $pushUpNumber $pushUpType"
+    var isChecked by remember { mutableStateOf(false) }
 
     Card(
         shape = MaterialTheme.shapes.medium,
@@ -90,10 +96,23 @@ fun PushUpItem(pushUp: PushUp, dayNumber: Int, modifier: Modifier = Modifier) {
             modifier = Modifier
                 .padding(dimensionResource(R.dimen.padding_medium))
         ) {
-            Text(
-                text = itemText,
-                style = MaterialTheme.typography.bodyLarge,
-            )
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = itemText,
+                    style = MaterialTheme.typography.bodyLarge,
+                )
+                CompositionLocalProvider(LocalMinimumInteractiveComponentEnforcement provides false) {
+                    Checkbox(
+                        checked = isChecked,
+                        onCheckedChange = {
+                            isChecked = !isChecked
+                        }
+                    )
+                }
+            }
             Spacer(modifier = Modifier.height(8.dp))
             Box(
                 modifier = Modifier
